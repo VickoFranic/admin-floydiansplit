7d2aba99e68b323bc85c2c57eb665dacc6e20d19
Vicko Franić <vicko@q-alliance.com>
2017-08-11 13:36:44 +0200

    Updated prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appId: '247396842448228',
-  apiUrl: 'http://floydiansplitapi.dev/api/'
+  apiUrl: 'https://floydiansplitapi.herokuapp.com/api/'
 };
